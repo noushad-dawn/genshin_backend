9f7360df86db03e77ad608dcd4c89ab061900938
Your Name <your-email@example.com>
2025-01-12 12:12:05 +0530

    final

--- index.js
+++ index.js
@@ -23,10 +23,8 @@ cloudinary.config({
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI
+);
 
 const db = mongoose.connection;
 db.once('open', () => console.log('Connected to MongoDB'));
